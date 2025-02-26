b536c9a1f379fc9fc33ed69932bc72de83093c8e
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2025-02-25 21:35:03 -0300

    Adiciona get by Id

--- src/app.js
+++ src/app.js
@@ -11,14 +11,25 @@ const listas = [
     {id: 2, nome: 'Bruno'},
 ]
 
+
+function buscaPorId(id) {
+    return listas.filter(listas => listas.id == id)
+}
+
 //rota raiz
 app.get('/', (req,res) => {
     res.status(200).send('Aula de Prog Web - Pro Bruno Nascimento - SEI')
 })
 
 //novas rotas
-app.get('/listas', (req,res) => {
+
+app.get('/listas/', (req,res) => {
     res.status(200).send(listas)
+})
+
+
+app.get('/listas/:id', (req,res) => {
+    res.json(buscaPorId(req.params.id))
 })
 
 app.get('/materias', (req,res) => {
